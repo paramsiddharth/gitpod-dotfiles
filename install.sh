2f8be378b5cf0f7c49daedb999e1dbcc06895215
Param Siddharth <contact@paramsid.com>
2022-02-27 14:56:15 +0530

    Configure Git

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# Add the keys
 [[ ! -z $GNUGPG  ]] &&
 cd ~ &&
 rm -rf .gnupg &&
 echo $GNUGPG | base64 -d | tar --no-same-owner -xzvf -
 
+# Set up the keys
 [[ ! -z $GNUPG_SIGNING_KEY  ]] &&
 git config --global user.signingkey $GNUPG_SIGNING_KEY &&
-git config commit.gpgsign true
+git config --global commit.gpgsign true
+
+# Configure Git
+git config --global user.name  $GIT_AUTHOR_NAME
+git config --global user.email $GIT_AUTHOR_EMAIL
