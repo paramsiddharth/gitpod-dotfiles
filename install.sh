fdf0fadd3a87ca9458b7a72e6e5e1c603638d813
Param Siddharth <contact@paramsid.com>
2022-02-27 15:00:35 +0530

    Fix environment variable passing

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ git config --global user.signingkey $GNUPG_SIGNING_KEY &&
 git config --global commit.gpgsign true
 
 # Configure Git
-git config --global user.name  $GIT_AUTHOR_NAME
-git config --global user.email $GIT_AUTHOR_EMAIL
+git config --global user.name  "$GIT_AUTHOR_NAME"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
